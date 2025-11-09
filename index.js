1580c57915a62093798aeb47b0da957d637d434e
Dhanuka <dhanuka200098@gmail.com>
2025-11-09 07:24:31 +0530

    updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://wp-frontend.vercel.app"],
   credentials: true,
 }));
 
